a8ada9d262c9aa82cb4c7758d1021dd567efada9
mluis210393 <mluis_21@hotmail.com>
2022-01-24 23:45:47 -0600

    se sube pokedex completo

--- script2.js
+++ script2.js
@@ -1,7 +1,4 @@
-
 var x = prompt("Ingrese el numero de pokemon o nombre del pokemón");
-var z = prompt("Ingrese segundo numero de pokemon o nombre del pokemón");
-var b = "https://pokeapi.co/api/v2/pokemon/" + z.toLowerCase(); + "/";
 
 var y ="https://pokeapi.co/api/v2/pokemon/" + x.toLowerCase(); + "/";
 
@@ -9,18 +6,9 @@ fetch(y)
 .then(respuesta => respuesta.json())
 .then(pokemon => {
   let element = document.getElementById('elemento')
-  element.innerHTML = `<p> Nombre: ${pokemon.name}<p>
-  <img src='${pokemon.sprites.front_default}'><p>${pokemon.height}<p><p>${pokemon.weight}<p>`;
+  element.innerHTML = `<p style="text-align: center;"><img src='${pokemon.sprites.front_default}' style="height: 350px; width: 350px;" <p><br><br><br><br><br><h1 style="top: -600px; left: 0px;">Nombre: git${pokemon.name}<h1><p><h2>Altura: ${pokemon.height}<p><p>Peso: ${pokemon.weight}<p>`;
   console.log(pokemon)
 })
 .catch(err=>console.log(err))
 
-fetch(b)
-.then(response => response.json())
-.then(pokemon2 => {
-  let element2 = document.getElementById('elemento2')
-  element2.innerHTML = `<p>${pokemon2.name}<p>
-  <img src='${pokemon2.sprites.front_default}'><p>${pokemon2.height}<p>`;
-  console.log(pokemon2)
-})
-.catch(err=>console.log(err))
+
---
+++ script3.js
@@ -0,0 +1,19 @@
+
+/*
+
+.eye{
+    position:absolute;
+    height:200 px;
+    width:200 px;
+    top: 40 px;
+    left : 40 px;
+    z-index: 1;
+  }
+  
+  .heaven
+   {
+    position:absolute;
+    height:300 px;
+    width:300 px;
+    z-index: -1;
+  }*/
